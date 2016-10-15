11231cbc71522226437508d9389b8f9fce4172b6
Dustin Biser <dustinbiser@gmail.com>
2016-10-15 11:41:51 -0400

    Removed DirectX dependencies Will add later if we decide to use DirectX

--- Assets/Shaders/PSInput.hlsli
+++
@@ -1,9 +0,0 @@
-//
-// PSInput.hlsli
-//
-
-struct PSInput
-{
-	float4 position : SV_POSITION;
-	float4 color : COLOR;
-};
--- Source/pch.h
+++ Source/pch.h
@@ -20,11 +20,14 @@
 
 #include <windows.h>
 
-#include <d3d12.h>
-#include <dxgi1_4.h>
-#include <D3Dcompiler.h>
-#include <DirectXMath.h>
-#include "d3dx12.h"
+/* Add these later if we use DirectX
+
+	#include <d3d12.h>
+	#include <dxgi1_4.h>
+	#include <D3Dcompiler.h>
+	#include <DirectXMath.h>
+	#include "d3dx12.h"
+*/
 
 #include <string>
 #include <wrl.h>
